9b8c1a5d371c9cf283f4bdfecda4e6825e065b63
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-02-06 16:03:22 -0200

    Write a code who read a value in meters, convert and show in millimeters.

---
+++ exerc_308.py
@@ -0,0 +1,7 @@
+"""
+Write a code who read a value in meters, convert and show in millimeters.
+"""
+value_meters = int(input('We are going to convert meters in millimeters. Enter, a with number: '))
+print('You type ', value_meters, 'm')
+converting_millimeters = (value_meters * 1000)
+print('The conversion has been finishing. You have', converting_millimeters, 'millimeters')
