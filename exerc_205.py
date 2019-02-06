db8c771eb074769444f8ba3cbcf57f448580b51e
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-02-06 15:34:26 -0200

    Alter file names.

---
+++ exerc_205.py
@@ -0,0 +1,7 @@
+"""
+Modify the program list_207.py, calculating the sum of three variables
+"""
+a = 8
+b = 4
+c = 18
+print(a + b + c)
--- exerc_25.py
+++
@@ -1,7 +0,0 @@
-"""
-Modify the program list_27.py, calculating the sum of three variables
-"""
-a = 8
-b = 4
-c = 18
-print(a + b + c)
