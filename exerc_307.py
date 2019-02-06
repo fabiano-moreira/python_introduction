a854e5f781f1557b7e7d05f021abcccbbbc897c7
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-02-06 15:41:10 -0200

    Create a code asking two integers. Print the sum of these numbers.

---
+++ exerc_307.py
@@ -0,0 +1,7 @@
+"""
+Create a code asking two integers. Print the sum of these numbers.
+"""
+number1 = int(input('Type the first integer number: '))
+number2 = int(input('Type the second integer number: '))
+sum_of_numbers = number1 + number2
+print('The sum of two numbers are: ', sum_of_numbers)
